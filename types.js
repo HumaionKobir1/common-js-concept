20ed1531f8d8d90291921e7507cdf552a3fc94e2
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-01 15:33:13 +0600

    Added => Module Overview, Primitive and non-primitive data type

---
+++ types.js
@@ -0,0 +1,24 @@
+// JavaScript is  a dynamic typed Language
+
+// primitive type
+const a = 5;
+const b = 'Humaion koibr';
+const d = true;
+
+
+// non-primitive
+const ages = [18, 24, 26, 32];
+const student = {id: 23, class: 7};
+
+// console.log(typeof a, typeof b, typeof ages, typeof student);
+
+let x = 5;
+let y = x;
+console.log(x, y);
+y = 7;
+console.log(x, y);
+
+let p = {job: 'Web developer'};
+let q = p;
+q.job = 'Front end developer';
+console.log(q);
