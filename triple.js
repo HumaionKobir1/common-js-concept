a12b73031a22147dde4bdb67c02f8bb6bf19453c
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-01 16:44:55 +0600

    Added => double equal (==) vs triple equal (===), implicit conversion

---
+++ triple.js
@@ -0,0 +1,19 @@
+
+// always use ===
+
+const first = 2;
+const second = true;
+if (first == second){
+    console.log('value are equal');
+}
+else{
+    console.log('value are not  equal');
+}
+
+
+if (first === second){
+    console.log('value are equal');
+}
+else{
+    console.log('value are not  equal');
+}
